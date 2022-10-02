474f3e66db3aed297073d77c49f6713f53977450
Keisuke-Magara <61485115+Keisuke-Magara@users.noreply.github.com>
2022-10-02 21:52:31 +0900

    uppdate test.py

--- .github/workflows/test.py
+++ .github/workflows/test.py
@@ -1,5 +1,7 @@
 def helloworld():
-    print("Hello World from Python!")
+    with open(file='../../pubspec.yaml') as f:
+        for line in f:
+            print(line)
 
 if __name__ == '__main__':
     helloworld()
